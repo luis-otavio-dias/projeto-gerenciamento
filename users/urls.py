30a907e234c6414f3a3df71560d7f6bf6a59278a
luis-otavio-dias <luis777dias231@gmail.com>
2025-04-07 20:29:22 -0300

    Configurando users.urls e project.urls para página inicial ser register.html

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from users import views
 app_name = "users"
 
 urlpatterns = [
+    path("", views.register, name="register"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
