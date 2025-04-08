e0ff135b8fa836dae0c4a2dd1fa91e69d3828a82
luis-otavio-dias <luis777dias231@gmail.com>
2025-04-08 15:41:43 -0300

    Ajustando tags e condicional na tag img em students.html e meeting.html, exibindo token do mentorado em task/student.id

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls")),
-    path("users/", include("users.urls")),
+    # path("users/", include("users.urls")),
     path("students/", include("students.urls")),
 ]
 
